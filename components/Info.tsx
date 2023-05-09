b2ba12e68b45151cf267ec46a8be43b93006f93e
Mayank <mayank.nice9411@gmail.com>
2023-05-10 00:36:56 +0530

    my first commit

--- components/Info.tsx
+++ components/Info.tsx
@@ -3,7 +3,7 @@ const Info = () => {
     return (
         <div className="p-6 block bg-[var(--bg-color)]  relative">
             <div className="absolute rounded-full bottom-[80%] right-[50%] translate-x-[50%] bg-gradient1 w-44 h-44 flex justify-center items-center">
-                <Image src="/logo-low-white-black.png" className="w-36 h-36 rounded-full " alt='profile' height={40} width={40} />
+                <Image src="/logo-low-white-black.png" className="w-36 h-36 rounded-full " alt='profile' height={300} width={300} />
             </div>
             <div className="leading-9 pt-10">
                 <h1 className="text-[2rem] text-bg-gradient font-extrabold mb-2">DateTech Software</h1>
